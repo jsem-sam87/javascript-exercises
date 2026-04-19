45b638a9bedb7d6507f5fed66a58116841416cbf
SamuelVins <samuel.vins87@gmail.com>
2026-04-19 10:33:43 +0200

    exercise08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...removeThis) {
+    return myArray.filter(singleOne => !removeThis.includes(singleOne));
 };
 
+
+// removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 // Do not edit below this line
 module.exports = removeFromArray;
