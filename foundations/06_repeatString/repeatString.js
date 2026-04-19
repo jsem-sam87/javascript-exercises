da45857b35e0d15cb03d2cccfe69f5f51eb9d730
SamuelVins <samuel.vins87@gmail.com>
2026-04-19 09:56:32 +0200

    exercise06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let res = "";
+    for (let i = 0; i < num; i++){
+        res += word;
+    }
+    return res;
 };
 
+
+// repeatString('hey', 3) // returns 'heyheyhey'
 // Do not edit below this line
 module.exports = repeatString;
