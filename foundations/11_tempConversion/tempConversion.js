bd5eaf5631e97e32aab64f30270d723be1f914dd
SamuelVins <samuel.vins87@gmail.com>
2026-04-19 11:28:58 +0200

    exercise11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  res = ((F - 32) / 1.8)
+  return Math.round(res * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+    res = ((C * 1.8) + 32)
+    return Math.round(res * 10) / 10;
 };
 
 // Do not edit below this line
