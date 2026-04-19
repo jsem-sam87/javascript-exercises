bc61deeaef644012f048ff529632e7621effafcd
SamuelVins <samuel.vins87@gmail.com>
2026-04-19 10:18:16 +0200

    exercise07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    return text.split("").reverse().join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
