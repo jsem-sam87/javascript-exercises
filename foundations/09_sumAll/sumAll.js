513ceb3d24a9e306324355f588e7d39feeedd5ae
SamuelVins <samuel.vins87@gmail.com>
2026-04-19 10:57:11 +0200

    exercise09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(lowest, highest) {
+    let res = 0;
 
+    if (lowest > highest) {
+        [lowest, highest] = [highest, lowest];
+    }
+    if (lowest % 1 != 0 && lowest % 1 != 0 || lowest < 0 || highest < 0 || !Number.isInteger(lowest) || !Number.isInteger(highest)){
+        return "ERROR";
+    }
+
+
+    for (i = lowest; i <= highest; i++){
+        res += i;
+    }
+    return res;
 };
 
 // Do not edit below this line
