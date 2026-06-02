e1a68b8e36e900d38927e5600b84ebffb8c5c77c
SamuelVins <samuel.vins87@gmail.com>
2026-06-02 18:34:36 +0200

    exercise 12 completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  return numArray.reduce((sum, addSum) => sum + addSum, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((sum, addSum) => sum * addSum); 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+  
+	for (let i = num; i > 0; i--)
+  {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
