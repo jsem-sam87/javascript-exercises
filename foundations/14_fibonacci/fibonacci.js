9a3cbeef9c2f0f3ed8414c4e31c258397fee1848
SamuelVins <samuel.vins87@gmail.com>
2026-06-02 19:23:27 +0200

    exercise 14 completed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    
+    if (member == 0) return 0;
+    if (member < 0) return "OOPS";
 
+    const mySequence = [1, 1];
+    for (let i = member; i > 2; i--)
+    {
+        mySequence.push(mySequence.at(-1) + mySequence.at(-2))
+    }
+
+    return mySequence.at(member - 1)
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
