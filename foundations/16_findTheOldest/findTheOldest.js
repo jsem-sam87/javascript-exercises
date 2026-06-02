76d10900c18f2aed3f216e457aea9e12a53df00b
SamuelVins <samuel.vins87@gmail.com>
2026-06-02 20:03:25 +0200

    exercise 16 completed

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestOne = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
 
-};
+        const currentOne = getAge(current.yearOfBirth, current.yearOfDeath)
+
+        return oldestOne > currentOne ? oldest : current;
+    })
+}
+
+const getAge = function(birth, death){
+    if (!death) {
+        death = new Date().getFullYear(); // Automatically grabs the current year
+    }
+    return death - birth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
