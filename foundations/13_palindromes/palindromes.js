d57e961dacc215bfe3472d6b47e0418c78791b1e
SamuelVins <samuel.vins87@gmail.com>
2026-06-02 18:50:55 +0200

    exercise 13 completed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (user_input) {
+    user_input1 = user_input.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    reversed = user_input1.toLowerCase().split('').reverse().join('');
+    return user_input1 === reversed ? true : false;
 };
 
 // Do not edit below this line
